1448923635f5702a83af2100c2f7cef6004742d9
Ferm11n <111800128+ferm11n@users.noreply.github.com>
2025-05-11 14:17:19 -0300

    agregamos barra de navegacion para mantener y gestionar los inicios de sesion de los diferentes usuarios

---
+++ js/componentes/barraNavegacion.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const user = JSON.parse(localStorage.getItem("usuarioActual"));
+    const navLogin = document.getElementById("navLogin");
+
+    if (user && navLogin) {
+        navLogin.textContent = "Cerrar sesión";
+        navLogin.href = "#";
+        navLogin.addEventListener("click", (e) => {
+            e.preventDefault();
+            localStorage.removeItem("usuarioActual");
+            // redireccionar a index o a login, lo que te guste
+            window.location.href = "index.html";
+        });
+    } else {
+        //Si no te logeas, se mantiene el "Entrar"
+        navLogin.textContent = "Entrar";
+        navLogin.href = "login.html";
+        userName.textContent = `Hola, ${usuarioActual.username}!`;
+    }
+});
