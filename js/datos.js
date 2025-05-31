9c2ec4c7b04fdc3429f61724c702076ee589ea27
Facundo-Diaz <faku98_lomas@hotmail.es>
2025-05-31 10:32:27 -0300

    me traigo cambios efectuados de la rama de lucas

---
+++ js/datos.js
@@ -0,0 +1,32 @@
+export const SALONES_INICIALES = [
+    {
+        id: 1,
+        titulo: "Salon Fiesta",
+        descripcion: "ideal para cumpleaños infantiles",
+        direccion: "Pepperoni 1765",
+        valor: 570,
+        estado: "Disponible",
+        imagen: "img/salones/salones-infantiles/salonInfantil1.jpg",
+        role: "salon"
+    },
+
+    {
+        id: 2,
+        titulo: "Salon Fiesta Dos",
+        descripcion: "ideal para cumpleaños infantiles dos",
+        direccion: "Albaca 1755",
+        valor: 1000,
+        estado: "Reservado",
+        imagen: "img/salones/salones-infantiles/salonInfantil2.jpg"
+    },
+
+    {
+        id: 3,
+        titulo: "Salon Fiesta Tres",
+        descripcion: "ideal para cumpleaños infantiles Tres",
+        direccion: "Tomatioli 175",
+        valor: 100,
+        estado: "Disponible",
+        imagen: "img/salones/salones-infantiles/salonInfantil3.jpg"
+    }
+];
