f9363fe0d307c5df639d9d839fd99660b3c4da7d
Damián Exequiel Cornejo <damiancorbit@gmail.com>
2025-06-01 14:59:02 -0300

    SalonesCatalogo.js funcional
    Salones Catalogo crea y visualiza las tarjetas + datos.js iniciales

---
+++ js/datos.js
@@ -0,0 +1,95 @@
+export const SALONES_INICIALES = [
+    {
+        id: 1,
+        titulo: "Salon Regular 1",
+        descripcion: "Espacio luminoso y colorido, ideal para juegos y actividades creativas.",
+        direccion: "Pepperoni 1765",
+        valor: 150,
+        estado: "Disponible",
+        imagen: "img/salones/salones-regulares/salonRegular1.jpg",
+        role: "salon"
+    },
+
+    {
+        id: 2,
+        titulo: "Salon Regular 2",
+        descripcion: "Ambiente tranquilo y acogedor, perfecto para lectura y talleres.",
+        direccion: "Albaca 1755",
+        valor: 400,
+        estado: "Reservado",
+        imagen: "img/salones/salones-regulares/salonRegular2.jpg"
+    },
+
+    {
+        id: 3,
+        titulo: "Salon Regular 3",
+        descripcion: "Sala amplia y versátil, pensada para eventos y dinámicas grupales.",
+        direccion: "Tomatioli 175",
+        valor: 700,
+        estado: "Disponible",
+        imagen: "img/salones/salones-regulares/salonRegular3.jpg"
+    },
+
+    {
+        id: 4,
+        titulo: "Salon Halloween",
+        descripcion: "Decoración tenebrosa y luces tenues, ideal para una noche de sustos.",
+        direccion: "Tomatioli 175",
+        valor: 1100,
+        estado: "Disponible",
+        imagen: "img/salones/salones-tematicos/salonTematicoHalloween.jpg"
+    },
+
+    {
+        id: 5,
+        titulo: "Salon Mitico",
+        descripcion: "Inspirado en leyendas y criaturas mágicas, perfecto para fiestas fantásticas.",
+        direccion: "Tomatioli 175",
+        valor: 800,
+        estado: "Disponible",
+        imagen: "img/salones/salones-tematicos/salonTematicoMitico.jpg"
+    },
+
+    {
+        id: 6,
+        titulo: "Salon de la Epoca",
+        descripcion: "Ambiente de época con estandartes y armaduras, digno de reyes y caballeros.",
+        direccion: "Tomatioli 175",
+        valor: 1350,
+        estado: "Disponible",
+        imagen: "img/salones/salones-tematicos/salonTematicoEpoca.jpg"
+    },
+
+    {
+        id: 7,
+        titulo: "Salon infantil espacial",
+        descripcion: "Embárcate en un asombroso viaje por las estrellas, donde te esperan luces brillantes y planetas misteriosos.",
+        direccion: "Tomatioli 175",
+        valor: 1200,
+        estado: "Disponible",
+        imagen: "img/salones/salones-infantiles/salonInfantil1.jpg"
+    },
+
+    {
+        id: 8,
+        titulo: "Salon infantil de videojuegos",
+        descripcion: "Colores vivos, juegos y diversión asegurada para los más peques.",
+        direccion: "Tomatioli 175",
+        valor: 1050,
+        estado: "Disponible",
+        imagen: "img/salones/salones-infantiles/salonInfantil2.jpg"
+    },
+
+    {
+        id: 9,
+        titulo: "Salon infantil con trampolines",
+        descripcion: "Aventura entre trampolines, toboganes y mucha imaginación.",
+        direccion: "Tomatioli 175",
+        valor: 700,
+        estado: "Disponible",
+        imagen: "img/salones/salones-infantiles/salonInfantil3.jpg"
+    },
+];
+
+
+window.SALONES_INICIALES = SALONES_INICIALES;
