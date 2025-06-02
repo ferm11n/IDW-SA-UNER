f5e5fc64de86f96a2574bdb8c6b4784d10e4c251
Ferm11n <111800128+ferm11n@users.noreply.github.com>
2025-06-02 12:37:05 -0300

    comentarios actualizados

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import {DATOS_INICIALES} from "./datos.js";
-
-if (!localStorage.getItem("halls")) {
-    localStorage.setItem("halls", JSON.stringify(DATOS_INICIALES));
-    localStorage.setItem("hallId", "2")
-}
